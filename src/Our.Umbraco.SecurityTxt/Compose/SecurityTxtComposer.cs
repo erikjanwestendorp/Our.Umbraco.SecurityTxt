dac92b2d28c4173ccd95ff1e293f7638528edaea
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2023-07-07 11:41:17 +0200

    Add middleware

---
+++ src/Our.Umbraco.SecurityTxt/Compose/SecurityTxtComposer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Our.Umbraco.SecurityTxt.Middleware;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
+
+namespace Our.Umbraco.SecurityTxt.Compose;
+public class SecurityTxtComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.Configure<UmbracoPipelineOptions>(options =>
+        {
+            options.AddFilter(new UmbracoPipelineFilter(
+                "Security Security.txt",
+                applicationBuilder => { applicationBuilder.UseMiddleware<SecurityTxtMiddleware>(); },
+                applicationBuilder => { },
+                applicationBuilder => { }
+            ));
+        });
+    }
+}
