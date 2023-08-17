9dda8ed5a1461038675cc166fee8e393a686dab1
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2023-08-17 18:44:04 +0200

    Start working on Multi Domain support

--- src/Our.Umbraco.SecurityTxt/Configuration.cs
+++ src/Our.Umbraco.SecurityTxt/Configuration.cs
@@ -5,7 +5,7 @@ public class Configuration
     public class SecurityTxtSettings
     {
         public bool AsSection { get; set; } = false;
-
         public string Path { get; set; } = SecurityTxtConstants.Settings.DefaultPath;
+        public bool MultiDomain { get; set; } = false;
     }
 }
